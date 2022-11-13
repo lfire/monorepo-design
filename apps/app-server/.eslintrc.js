84d38fc0099e35bb3b0e34f4b1606c79e4a7b63d
benjilli <benjilli@tencent.com>
2022-11-13 22:41:03 +0800

    feat: finish init project version v1.x by lerna + pnpm

---
+++ apps/app-server/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  extends: ['@lerna/eslint-config-monorepo'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+};
---
+++ libs/lib-utils/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  extends: ['@lerna/eslint-config-monorepo/ts'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+};
